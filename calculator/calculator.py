8e1a6d2039ef4c2dc6be2299262d2e3e8ac4f875
Zahid Khan <zahidkhan18100@gmail.com>
2026-05-11 22:09:42 +0530

    Fix typo in operations variable name
    Corrected a typo in the variable name 'operetions' to 'operations'.

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -2,7 +2,7 @@
 #Input from the user
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
-operetions = input("Choose any operetions (/,*,+,-): ")
+operations = input("Choose any operetions (/,*,+,-): ")
 
 #adding conditionals 
 
